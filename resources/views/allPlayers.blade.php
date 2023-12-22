5906eb1ccb780422df8c60793d10b00bb47c8638
millatur <minnatur7373@gmail.com>
2023-12-23 00:23:50 +0600

    last commit me

--- resources/views/allPlayers.blade.php
+++ resources/views/allPlayers.blade.php
@@ -1,13 +1,47 @@
-<h1> All Users details list</h1>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MyTest</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1> All Users details list</h1>
 
 
-@foreach ($data as $id => $user)
+    <div class="container">
+        <div class="row">
+            <div class="col-7">
+              <table class="table table-bordered table-striped">
+                @foreach ($data as $id => $user)
+                    <tr>
+                        <td>{{$user->name}} </td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->city}}</td>
 
-<h3>
-    {{$user->name}} |
-    {{$user->email}} |
-    {{-- {{$user->age}} | --}}
-    {{$user->city}} |
-</h3>
+                    </tr>
+                    @endforeach
+              </table>
+            </div>
+        </div>
+    </div>
 
-@endforeach
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
