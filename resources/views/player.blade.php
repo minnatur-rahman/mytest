62ec5a3346739a6e30a0a3a5a82b980581217fd3
millatur <minnatur7373@gmail.com>
2023-12-24 06:33:39 +0600

    tosay second commit

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -1,1 +1,8 @@
 <h1>Single User Datils</h1>
+
+@foreach ($data as $id => $player)
+     <h3>Name : {{$player->name}}</h3>
+     <h3>Email : {{$player->email}}</h3>
+     <h3>Age : {{$player->age}}</h3>
+     <h3>City : {{$player->city}}</h3>
+@endforeach
